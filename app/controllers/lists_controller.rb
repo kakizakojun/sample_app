a0251595ac163c1ebeed214bd35c966e87f4e304
JunKakizako <111491185+kakizakojun@users.noreply.github.com>
2022-09-06 16:53:01 +0000

    [add]画像投稿機能の追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -31,8 +31,8 @@ class ListsController < ApplicationController
 
   private
   def list_params
-    params.require(:list).permit(:title,:body)
+    params.require(:list).permit(:title,:body,:image)
   end
 
-  
+
 end
